0d5c9f4c08069ac3df5f46aa5b4cd05f2e8852dd
sbesombes <seb@seb-Default-string>
2021-04-28 22:50:15 +0200

    Port to army 1.0.0

--- cmake/stream.cmake
+++ cmake/stream.cmake
@@ -1,13 +1,11 @@
-message("stream library path ${LIBRARY_PATH}")
-
 list(APPEND sources
-	${LIBRARY_PATH}/src/stream.cpp
-	${LIBRARY_PATH}/src/binarystream.cpp
-	${LIBRARY_PATH}/src/textstream.cpp
-	${LIBRARY_PATH}/src/outputstream.cpp
-	${LIBRARY_PATH}/src/stringstream.cpp
+	${PACKAGE_PATH}/src/stream.cpp
+	${PACKAGE_PATH}/src/binarystream.cpp
+	${PACKAGE_PATH}/src/textstream.cpp
+	${PACKAGE_PATH}/src/outputstream.cpp
+	${PACKAGE_PATH}/src/stringstream.cpp
 )
 
 include_directories(
-	${LIBRARY_PATH}/src
+	${PACKAGE_PATH}/src
 )
